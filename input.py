bc543af77979df471c6bfb68fb8b00b36b2c8dd9
HMMK <hmwemyamyakyaing@gmail.com>
2025-04-04 13:47:23 +0900

    first commit

---
+++ input.py
@@ -0,0 +1,15 @@
+# print("hello world"); # print hello world to console
+# print(input("Name : "));
+
+# name = input('Name :');
+# print ('hello '+ name);
+
+# age = input('how old are you?');
+# print('Your age is '+ age);
+
+# area calculator (pi r**2)
+PI = 3.14;
+r = int(input ('radius: '));
+
+area = PI* r**2;
+print(area);
