7a932de940f48098f9182b1628f278a6e0d4eb22
HMMK <hmwemyamyakyaing@gmail.com>
2025-04-07 12:51:32 +0900

    added range.py

---
+++ range.py
@@ -0,0 +1,21 @@
+# nums = [0,1,2,3,4];
+
+# for num in nums : 
+#     print(num)
+
+# for num in range(0,5) :
+# for num in range(5) :
+# for num in range(2,4) :
+# for num in range(0,20,5) :
+#     print(num)
+
+pizzas = ['cheesy', 'spicy', 'hawaian']
+
+for pizza in pizzas:
+    print(pizza)
+
+print(len(pizzas))
+
+for num in range(len(pizzas)) :
+    print(pizzas[num])
+
