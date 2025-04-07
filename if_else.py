80f22c18ff3a2d90651a2c5114120e597b7f8c93
HMMK <hmwemyamyakyaing@gmail.com>
2025-04-07 12:13:01 +0900

    Upload HomeWork

--- if_else.py
+++ if_else.py
@@ -9,11 +9,21 @@
 
     # print('hi'); 
 
-tired=input('are u tired? "y/n"') # y or n
+# tired=input('are u tired? "y/n"') # y or n
 
-if tired == 'y' :
-    print('rest well');
-elif tired =='n' :
-    print('go back to work');
-else : 
-    print('pls enter y or n');
+# if tired == 'y' :
+#     print('rest well');
+# elif tired =='n' :
+#     print('go back to work');
+# else : 
+#     print('pls enter y or n');
+
+user_name = "hmmk";
+password = "password12345";
+enter_username = input("Please enter your username : ");
+enter_password = input("Please enter your password :");
+
+if (enter_username == user_name) and (enter_password == password) :
+    print("You have logged in successfully!");
+else: 
+    print("Login Failed!");
