5a63d06ab647d6c24b6e3b5ad96536663696bb7a
HMMK <hmwemyamyakyaing@gmail.com>
2025-04-07 17:04:52 +0900

    added dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+person = {
+    'name' : 'hmmk',
+    'age'  : 32
+}
+
+# print(person['name'])
+# print('name' in person)
+
+person_keys = list(person.keys())
+print(person_keys)
+
+person_values = list(person.values())
+print(person_values)
