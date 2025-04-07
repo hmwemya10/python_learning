b480742bbd17eb5c109acbf9b39082acf803510a
HMMK <hmwemyamyakyaing@gmail.com>
2025-04-07 12:26:05 +0900

    Added loops.py

---
+++ loops.py
@@ -0,0 +1,17 @@
+names = ['Aung Aung', "Richard", "Tanaka", "Smith"]
+
+# for name in names : 
+#     print(name)
+
+# for name in names : 
+#     if name == 'Richard' :
+#         print(f'{name} is instructor')
+#         break
+#     else : 
+#         print(f'{name} is student')
+
+fruits = ['Guava', "Grapes", "Black-berries", "Strawberry"]
+
+for fruit in fruits :
+    print(f'{fruit} is fruit') 
+
