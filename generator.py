732714968a2890722ab9f554cc6f6b0517bf9074
HMMK <hmwemyamyakyaing@gmail.com>
2025-04-10 12:33:45 +0900

    mixing custom word in gibberish generator

--- generator.py
+++ generator.py
@@ -1,9 +1,20 @@
+custom_words = ['red', 'green', 'yellow', 'pink', 'blue']
+from random import randint
+
+def randomSentenceGenerator(word) :
+    randomIndex = randint(0,len(custom_words)-1)
+    return f'{custom_words[randomIndex]} {word}'
+
+
 with open('./text.txt') as file:
     parag = file.read()
+    word_list = parag.split() #['&asdf', 'asdf']
+    setence_list = list(map(randomSentenceGenerator,word_list))
+    paraCount = int(input('paragraph count :'))
 
-    ParaCount = int(input('para count: '))
-    for count in range(ParaCount):
+    for count in range(paraCount):
         with open('./generator.txt', 'a') as writefile :
-            writefile.write(parag+'\n\n')
+            writefile.write(''.join(setence_list)+'\n\n')
 
- 
+
+
